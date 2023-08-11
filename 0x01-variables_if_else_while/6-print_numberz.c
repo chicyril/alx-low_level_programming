2aabc0f51555c3e48ebee64fefd802670d2bdb59
cyril <cyrildaniels21+git@gmail.com>
2023-08-11 20:51:43 +0100

    add task 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints base 10 digit from 0 to 9 innclusive
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
