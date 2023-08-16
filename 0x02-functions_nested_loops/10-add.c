6086edc13e041093804efd30cb4e9626323366da
cyril <cyrildaniels21+git@gmail.com>
2023-08-16 04:59:16 +0100

    add task 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - sum of 2 int
+ * @num1: param1
+ * @num2: param2
+ *
+ * Return: sum
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
