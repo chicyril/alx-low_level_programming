9a970f8f775403c9765ed55bb3e044c094a524b8
cyril <cyrildaniels21+git@gmail.com>
2023-08-16 04:42:47 +0100

    add task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - print time table
+ *
+ */
+void times_table(void)
+{
+	int a, b, m;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			m = a * b;
+			_putchar(m);
+			if (b = '9')
+			{
+				_putchar('$');
+				continue;
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
