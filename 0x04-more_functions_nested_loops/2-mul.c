35495cddddf0c747b1e837a9e316457966359860
cyril <cyrildaniels21+git@gmail.com>
2023-08-17 22:51:32 +0100

    add task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @x: first number
+ * @y: second number
+ *
+ * Return: 0 on success
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
