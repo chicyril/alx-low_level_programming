e54fb4ceedee8452a2d1b8a2f84a5e3e87b88f42
cyril <cyrildaniels21+git@gmail.com>
2023-08-18 02:21:20 +0100

    add task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints square using ash sybole
+ * @size: size of the square
+ *
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
