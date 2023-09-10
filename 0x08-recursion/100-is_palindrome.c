516b2a98e5dda67191f62d7ee1c9392f2c18b6ed
cyril <cyrildaniels21+git@gmail.com>
2023-09-10 17:16:03 +0100

    modify task 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,21 +1,36 @@
 #include "main.h"
+
+/**
+ * chklen - ckeck the length of a string from an initial length
+ * @s: string
+ * @len: starting length
+ *
+ * Return: calculated length
+ */
+int chklen(char *s, int len)
+{
+	if (!s[len])
+		return (len);
+	len = chklen(s, len + 1);
+	return (len);
+}
 /**
  * tenet - checks is a string is a palindrome
  * @s: string to check
- * @lnt: length of the string
+ * @len: length of the string
  * @p: another pointer to string
  *
  * Return: 1 if palindrome or 0
  */
-int tenet(char *s, int lnt, char *p)
+int tenet(char *s, int len, char *p)
 {
 	int chk = 1;
 
 	if (*p == '\0')
-		return (chk);
-	chk = tenet(s, lnt - 1, p + 1);
+		return (1);
+	chk = tenet(s, len - 1, p + 1);
 	if (chk)
-		chk = (*p == s[lnt - 1]);
+		chk = (*p == s[len - 1]);
 	return (chk);
 }
 
@@ -28,12 +43,8 @@ int tenet(char *s, int lnt, char *p)
 int is_palindrome(char *s)
 {
 	char *p;
-	int lnt = 0;
+	int len = chklen(s, 0);
 
-	while (s[lnt] != '\0')
-		lnt++;
-	if (lnt == 0 || lnt == 1)
-		return (1);
 	p = s;
-	return (tenet(s, lnt, p));
+	return (tenet(s, len, p));
 }
